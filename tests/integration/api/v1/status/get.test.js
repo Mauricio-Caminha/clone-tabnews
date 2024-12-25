791f46e83d6e68f1fe1ab3915232054724a68deb
Maurício Caminha <mauricio.caminha@hotmail.com>
2024-12-25 10:25:32 -0300

    finaliza endpoint `/api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.current_connections).toEqual(1);
+});
+
+test.skip("SQL Injection", async () => {
+  await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+  await fetch(
+    "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+  );
 });
