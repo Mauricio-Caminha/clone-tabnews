efc07e1f182e4e58f13064c9e989a29139ed497a
Maurício Caminha <mauricio.caminha@hotmail.com>
2025-03-05 15:45:07 -0300

    feat: add `ServiceError` in `error.js`

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,31 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("An internal server error ocurred.", {
       cause,
     });
     this.name = "InternalServerError";
     this.action = "Get in touch with the support team.";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service unavailable at this moment.", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Verify if the service is up and running.";
+    this.statusCode = 503;
   }
 
   toJSON() {
